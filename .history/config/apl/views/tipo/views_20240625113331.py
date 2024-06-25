cba5e907464bb6e174dcede7621330751466daf3
unknown <fabiansarmientoglv@gmail.com>
2024-06-25 12:29:21 -0500

    en proceso de el crud

---
+++ .history/config/apl/views/tipo/views_20240625113331.py
@@ -0,0 +1,30 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.views.generic import ListView
+from django.shortcuts import render,redirect
+from apl.models import *
+
+
+def lista_tipo(request):
+    nombre={
+        'titulo': 'listado de categorias',
+        'nombre': Tipo.objects.all()
+    }
+    return render(request,'')
+    
+    
+class TipoListView(ListView):
+    model = Tipo
+    template_name = 'tipo/listar.html'
+    
+    
+    def dispatch(self, request, *args, **kwargs):
+        
+        
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['titulo'] = 'Listado de tipos'
+        return context

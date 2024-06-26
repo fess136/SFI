a8a9cd80653ead071726015cd6261ff25a69f6e2
Felipe Hurtado Mendez <andy16mendez@gmail.com>
2024-06-26 12:17:22 -0500

    Se crearon views de Empleados y Metodos de pago

--- config/apl/urls.py
+++ config/apl/urls.py
@@ -4,6 +4,9 @@ from apl.views.tipo.views import *
 from apl.views.marca.views import *
 from apl.views.presentacion.views import*
 from apl.views.administrador.views import*
+from apl.views.empleado.views import *
+from apl.views.metodo_pago.views import *
+
 app_name = 'apl'
 urlpatterns = [
 
@@ -12,6 +15,9 @@ urlpatterns = [
     path('tipo/crear', TipoCreateView.as_view(), name = "crear_tipo"),
     path("presentacion/listar",PresentacionListView.as_view(), name="listar_presentacion"),
     path('marcas/listar', MarcaListView.as_view(), name = "listar_marca"),
-    path('administrador/listar', AdministradorListView.as_view(), name="listar_administrador")
+    path('administrador/listar', AdministradorListView.as_view(), name="listar_administrador"),
+    path('empleado/listar', EmpleListView.as_view(), name="listar_empleado"),
+    path('metodos/listar', MetodosListView.as_view(), name="listar_metodo"),
+
 
 ]

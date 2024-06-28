b77a36eaaa3c66cffb58d4b9309c6ab6b5dd3709
Oscar <oscarhappy456@gmail.com>
2024-06-27 19:07:57 -0500

    No hay cambios relevantes

--- config/apl/tests.py
+++ config/apl/tests.py
@@ -4,6 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 from config.wsgi import*
 from apl.models import*
+
 # Create your tests here.
 
 p = Productos(nombre="zz",cantidad=100,precio=500,marcas_id=1,presentacion_id=1,tipo_id=1,unidad_medida_id=1).save()

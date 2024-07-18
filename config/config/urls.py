990e7dde539d646f1785ab4c81c468ef46e23ece
Felipe <andy16mendez@gmail.com>
2024-07-18 16:44:23 -0500

    Se agrego funcionamiento al reset de contraseña

--- config/config/urls.py
+++ config/config/urls.py
@@ -28,6 +28,7 @@ from django.contrib import admin
 from django.urls import path ,include
 from login.views import *
 from inicio.views import indexView
+from django.contrib.auth import views as auth_views
 
 from apl.views import *
 
@@ -36,7 +37,14 @@ urlpatterns = [
     path('', indexView.as_view(), name='index'),
     path('login', include('login.urls')),
     path('admin/', admin.site.urls),
-    path("apl/", include("apl.urls"))
-
+    path("apl/", include("apl.urls")),
     
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='register/password_reset_form.html'), name='reset_password'),
+
+    path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(template_name='register/password_reset_done.html'), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='register/password_reset_confirm.html'), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='register/password_reset_complete.html'), name='password_reset_complete'),
+
 ]

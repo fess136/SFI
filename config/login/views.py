a888387b389912feef0bf645047ccd5cd27f75f6
Grupo3 <fabiansarmientoglv@gmail.com>
2024-07-22 18:03:19 -0500

    se creo el usuario y se hicieron mejoras esteticas en el home

--- config/login/views.py
+++ config/login/views.py
@@ -7,31 +7,35 @@ from django.views.generic import RedirectView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
-# Create your views here.
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 class LoginFormView(LoginView):
-    template_name = 'login.html'
+     
+     template_name = 'login.html'
+     def loginview(request):
+          if request.method == 'POST':
+               form = LoginForm(request.POST)
+               if form.is_valid():
+                    username = form.cleaned_data.get('username')
+                    password = form.cleaned_data.get('password')
+               user = authenticate(request, username=username, password=password)
+               if user is not None:
+                    login(request, user)
+                    return redirect('apl:listar_tipo')  # Redirige a la página de inicio después del login
+               else:
+                    form.add_error(None, 'Nombre de usuario o contraseña incorrectos')
+          else:
+               form = LoginForm()
 
-    def dispatch(self, request, *args, **kwargs):
-         if request.user.is_authenticated:
-          return redirect('apl:listar_tipo' )
-         return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-         context = super().get_context_data(**kwargs)
-         context['titulo']='iniciar sesion'
-           
-         return context
-    
-
+          return render(request, 'login.html', {'form': form})
 
 class logoutredirect(RedirectView):
-     pattern_name = 'login'
-
-     def dispatch(self, request,  *args, **kwargs):
-         
+     
+     pattern_name="login"  
+     
+     def dispatch(self, request, *args, **kwargs) :
           logout(request)
-          return super().dispatch (request, *args, **kwargs)
-
-
-    
+          
+          return super().dispatch(request, *args, **kwargs)

ae9ff76db8f03716d29ea91d68c10d017ef1d398
Oscar <oscarhappy456@gmail.com>
2024-07-25 17:52:02 -0500

    Se implemento el detalle de la compra v.1.1

--- config/apl/tests.py
+++ config/apl/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 import sys
 from pathlib import Path
+
+from django.urls import reverse_lazy
 sys.path.append(str(Path(__file__).parent.parent))
 from config.wsgi import*
 from apl.models import*
@@ -8,10 +10,11 @@ from apl.models import*
 # Create your tests here.
 
 p = Productos(nombre="zz",cantidad=100,precio=500,marcas_id=1,presentacion_id=1,tipo_id=1,unidad_medida_id=1).save()
+# a = 
+print([i[0] for i in list(Compras.objects.values_list('id', flat=False))][-1])
 try:
 
         p = Productos.objects.get(id=4)
-        print(p.nombre)
                 
         # p.nombre = "loca"
         # p.save()
--- config/apl/views/detallecompra/views.py
+++ config/apl/views/detallecompra/views.py
@@ -7,27 +7,24 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 
+from apl.forms import DetalleCompraForm
 
-class DetalleCompraListView(ListView):
-
-    model = DetalleCompra
-    template_name = "DetalleCompra/listar.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['titulo'] = "Detalle de Compra"
-        context['crear_url'] = reverse_lazy('apl:crear_detallecompra')
-        return context
 
 class DetalleCompraCreateView(CreateView):
 
     model = DetalleCompra
+    form_class = DetalleCompraForm
     template_name = "DetalleCompra/crear.html"
-    success_url = reverse_lazy("apl:listar_detallecompra")
+
+    def get_success_url(self):
+        # Obtén el último ID de la tabla Compras
+        ultimo_id = Compras.objects.values_list('id', flat=True).last()
+        return reverse_lazy('apl:detallar_detallecompra', args=[ultimo_id])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = "Crear Detalle de Compra"
+        return context
 
 class DetalleCompraDeleteView(DeleteView):
 
@@ -42,6 +39,13 @@ class DetalleCompraDeleteView(DeleteView):
 
 class DetalleCompraDetailView(DetailView):
 
-    model = DetalleCompra
+    model = Compras
     template_name = "DetalleCompra/listar.html"
+    context_object_name = "objeto"
     success_url = reverse_lazy("apl:listar_compra")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['crear_url'] = reverse_lazy('apl:crear_detallecompra')
+
+        return context
